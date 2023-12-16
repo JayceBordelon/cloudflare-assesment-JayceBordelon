67d769d390c500ed909c74ebe0b76b9c7d323d76
Jayce Bordelon <jaycebordelon@Jayces-MacBook-Pro.local>
2023-12-16 12:26:08 -0600

    build test

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,15 @@ function App() {
 
   return (
     <>
-      <h1>Hello</h1>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Hello Booper</h1>
     </>
   )
 }
